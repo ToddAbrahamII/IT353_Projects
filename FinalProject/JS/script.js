acaf438d6af5a551d72851b23c7a0074f6e3f17d
Todd Abraham <tjabraham12@yahoo.com>
2024-04-19 09:29:29 -0500

    Project

--- FinalProject/JS/script.js
+++ FinalProject/JS/script.js
@@ -6,45 +6,5 @@ document.addEventListener('DOMContentLoaded', function() {
         navbarMenu.classList.toggle('show');
     });
 
-    let currentScenario = 1;
-    let changeTimeout;
-
-    function changeScenario(offset) {
-        clearTimeout(changeTimeout);
-
-        const totalScenarios = 3;
-        currentScenario += offset;
-
-        if (currentScenario > totalScenarios) {
-            currentScenario = 1;
-        } else if (currentScenario < 1) {
-            currentScenario = totalScenarios;
-        }
-
-        updateScenario();
-
-        // Set change after 6 seconds
-        changeTimeout = setTimeout(() => {
-            changeScenario(1);
-        }, 6000);
-    }
-
-    function updateScenario() {
-        const contents = document.querySelectorAll('.Announcement');
-        contents.forEach(content => {
-            const scenario = parseInt(content.dataset.scenario);
-            if (scenario === currentScenario) {
-                content.classList.add('active');
-            } else {
-                content.classList.remove('active');
-            }
-        });
-    }
-
-    updateScenario();
-
-    // Initial timeout for automatic change after 6 seconds
-    changeTimeout = setTimeout(() => {
-        changeScenario(1);
-    }, 6000);
+   
 });
